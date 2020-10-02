95c522cf32743d5ebae1f91b85474c84b81aa657
Nevyr2 <clement.davin@epita.fr>
2020-10-02 18:57:59 +0200

    modify front end to add toolbar and navbar

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
-var express = require('express');  
+var express = require('express');
 var app = express();
-var server = require('http').createServer(app);  
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '../node_modules'));  
+app.use(express.static(__dirname + '../node_modules'));
 app.get('/', function(req, res,next) {  
     res.sendFile(__dirname + '/index.html');
 });
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const log = console.log;    
+const log = console.log;
+var fullText = ""
 
 server.listen(port, hostname, () => log(`Server running at http://${hostname}:${port}/`))
 io.on('connection', (socket) => {
     log('connected')
+	socket.broadcast.emit('message', fullText)
     socket.on('message', (evt) => {
-        log(evt)
+		fullText = evt
         socket.broadcast.emit('message', evt)
     })
 })
